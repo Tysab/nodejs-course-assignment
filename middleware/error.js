371d06e513aa4715be9b409ccd3ff9be96316e42
unknown <tylerbroere@gmail.com>
2019-12-06 13:47:26 +0100

    fixed deprecated winston bug

--- middleware/error.js
+++ middleware/error.js
@@ -10,7 +10,7 @@ module.exports = function (err, req, res, next) {
     //  silly
 
     //  Log the exception
-    winston.error(err);
+    winston.error(err.message, err);
 
 
     res.status(500).send(`Something went wrong: ${err.message}`);
--- startup/logging.js
+++ startup/logging.js
@@ -5,32 +5,46 @@ require('express-async-errors');
 
 module.exports = function (key) {
 
-    // process.on('uncaughtException', (ex) => {
-    //     winston.error(ex.message, ex);
-    //     process.exit(1);
-    // });
-
-    winston.handleExceptions(
-        new winston.transports.Console({
-            colorize: true,
-            prettyPrint: true
-        }),
-        new winston.transports.File({
-            filename: 'uncaughtExceptions.log'
-        })
-    );
+    //  error
+    //  warn
+    //  info
+    //  verbose
+    //  debug
+    //  silly
 
     process.on('unhandledRejection', (ex) => {
         throw ex;
     });
 
-    winston.add(new winston.transports.File({
-        filename: 'logfile.log'
+    winston.add(new winston.transports.Console({
+        level: 'info',
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`),
+        ),
+        handleExceptions: true
     }));
+
+    winston.add(new winston.transports.File({
+            filename: 'uncaughtExceptions.log',
+            level: 'error',
+            handleExceptions: true
+        }),
+        new winston.transports.File({
+            filename: 'logfile.log',
+            level: 'info'
+        }));
+
 
     winston.add(new winston.transports.MongoDB({
         db: key,
-        level: 'info'
+        level: 'info',
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`),
+        ),
+        handleExceptions: true
     }));
 
     // //throw new Error('Index.js error');
